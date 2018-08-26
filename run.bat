d1aeebdbe133a47eba69b759cd88e726999f8bf2
Krishna Prasad <skprasadu@yahoo.com>
2018-08-26 09:54:04 -0700

    new project

---
+++ run.bat
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=target\tabula-1.0.4-SNAPSHOT-jar-with-dependencies.jar -DgroupId=technology.tabula -DartifactId=tabula -Dversion=1.0.4-SNAPSHOT -Dpackaging=jar -DpomFile=pom.xml
